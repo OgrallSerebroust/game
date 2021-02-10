2e130d073c8efe5978a3a65db03007730198d7f3
howtus <howtus12@gmail.com>
2021-02-10 20:42:35 +0300

    Added player's constants

--- src/mire/player.clj
+++ src/mire/player.clj
@@ -1,10 +1,43 @@
 (ns mire.player)
 
-(def ^:dynamic *current-room*)
-(def ^:dynamic *inventory*)
-(def ^:dynamic *name*)
+(def existing-items (ref #{}))
 
+; комната в которой находися игрок
+(def ^:dynamic *current-room*)
+; инветраь игрока
+(def ^:dynamic *inventory*)
+; имя игрока
+(def ^:dynamic *name*)
+; id игрока
+(def ^:dynamic *id*)
+; колличество ключей
+(def ^:dynamic *keys-count* (ThreadLocal.))
+; хп всех игроков
+(def health (ref {}))
+; атаки всех игроков
+(def attack-values (ref {}))
+;  очки игроков
+(def scores (ref {}))
+
+; учёт собранных сетов
+(def ^:dynamic *sets*)
+
+; константы игрока
+; константа с максимальным хп
+(def max-health 50)
+; занчение базовой атаки
+(def base-attack-value 25)
+; время возрождения игрока
+(def respawn-time 100)
+; очки за убийства игрока
+(def points-for-kill 5000)
+
+; Constants
 (def prompt "> ")
+(def eol (System/getProperty "line.separator"))
+(def target-score 20000)
+(def finished (atom false))
+
 (def streams (ref {}))
 
 (defn carrying? [thing]
